2d22835136a7fdbb22b30b6c0cac5e6ce7ff1916
Vitalik <vitaliktuhata@gmail.com>
2024-11-05 22:03:37 +0200

    1

---
+++ website/src/main/java/scherbatyuk/website/controllers/StartController.java
@@ -0,0 +1,14 @@
+package scherbatyuk.website.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StartController {
+
+@GetMapping("/start")
+    public String startPage(){
+
+    return "start";
+}
+}
